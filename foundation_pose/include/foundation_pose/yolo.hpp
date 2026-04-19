0d98204d287773c41553bb2407aba1b81dc1c449
Sachin Kumar <sachinkum123567@gmail.com>
2026-04-19 19:49:01 +0200

    Implement YOLO object detection node and update CMake configuration

--- foundation_pose/include/foundation_pose/yolo.hpp
+++ foundation_pose/include/foundation_pose/yolo.hpp
@@ -4,22 +4,35 @@
 #include "foundation_pose/trt_engine.hpp"
 #include <opencv4/opencv2/opencv.hpp>
 #include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.hpp>
 
 namespace yolo
 {
-    class Yolo
+    class Yolo : public rclcpp::Node
     {
     public:
+        /**
+         * @brief Constructor for the YOLO
+         * * Initializes the class
+         * @param model_path Path to the exported YOLO model file (.onnx, or .engine)
+         */
         Yolo(const std::string &model_path);
         ~Yolo();
 
         /**
-         * @brief Run inference to detect objects
+         * @brief Performs object detection on the input image.
+         * @param img The input frame (typically BGR from OpenCV). The function
+         * * automatically handles resizing and normalization.
          */
         void run_inference(const cv::Mat &img);
 
     private:
+        void image_cb(sensor_msgs::msg::Image::ConstSharedPtr msg);
         std::unique_ptr<foundation_pose::TrtEngine> _trt_engine;
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr _img_raw_sub;
+        rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _img_res_pub;
     };
 } // namespace yolo
 
--- foundation_pose/src/yolo.cpp
+++ foundation_pose/src/yolo.cpp
@@ -2,12 +2,44 @@
 
 namespace yolo
 {
-    Yolo::Yolo(const std::string &model_path) {
-        auto _trt_engine = std::make_unique<foundation_pose::TrtEngine>(model_path);
+    Yolo::Yolo(const std::string &model_path) : Node("yolo_ros_node")
+    {
+        _trt_engine = std::make_unique<foundation_pose::TrtEngine>(model_path);
+        _img_raw_sub = this->create_subscription<sensor_msgs::msg::Image>("img_in", 10, std::bind(&Yolo::image_cb, this, std::placeholders::_1));
+        _img_res_pub = this->create_publisher<sensor_msgs::msg::Image>("img_out", 10);
     }
     Yolo::~Yolo() {}
 
-    void Yolo::run_inference(const cv::Mat &img) {
+    void Yolo::run_inference(const cv::Mat &img)
+    {
         _trt_engine->infer(img);
     }
+
+    void Yolo::image_cb(sensor_msgs::msg::Image::ConstSharedPtr msg)
+    {
+        // convert img to opencv
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            // Use bgr8 for standard OpenCV color format
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (const cv_bridge::Exception &e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        run_inference(cv_ptr->image);
+    }
 } // namespace yolo
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    std::string model_path = "/home/asus/backup/zzzzz/ros2/personal_projects/vision-flow/foundation-pose-trt/yolo26l_rtx3060.engine";
+    auto node = std::make_shared<yolo::Yolo>(model_path);
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
